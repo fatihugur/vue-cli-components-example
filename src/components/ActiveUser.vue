dc53da62ad64cd10bf93d71ffa7ead036ed90ffd
Fatih Ugur <fatihugur@gmail.com>
2021-02-01 16:22:23 +0100

    UserData.vue and ActiveUser.vue and CSS styling added

---
+++ src/components/ActiveUser.vue
@@ -0,0 +1,17 @@
+<template>
+  <section>
+    <h2>Fatih Ugur</h2>
+    <h3>45 Years</h3>
+  </section>
+</template>
+<script>
+export default {};
+</script>
+<style>
+section {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  border: 1px solid #ccc;
+}
+</style>
---
+++ src/components/UserData.vue
@@ -0,0 +1,11 @@
+<template>
+  <section>
+    <label>Name: </label>
+    <input type="text" placeholder="Your name" />
+    <label>Age: </label>
+    <input type="text" placeholder="Your age" />
+  </section>
+</template>
+<script>
+export default {};
+</script>
